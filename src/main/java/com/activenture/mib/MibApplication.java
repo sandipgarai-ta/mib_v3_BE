e030060700619bb6c0549bfbf1a7f7fade406c4e
Sandip Garai <sandip.garai.developer@gmail.com>
2018-10-02 18:18:10 +0530

    Project setup completed

---
+++ src/main/java/com/activenture/mib/MibApplication.java
@@ -0,0 +1,12 @@
+package com.activenture.mib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MibApplication.class, args);
+	}
+}
---
+++ src/test/java/com/activenture/mib/MibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.activenture.mib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
