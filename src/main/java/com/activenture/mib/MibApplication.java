fa919c846ff70e84c7c3c3a2b99e0708fd80596f
sandip-garai-developer <sandip.garai.developer@gmail.com>
2019-01-20 12:35:58 +0530

    war convert

--- src/main/java/com/activenture/mib/MibApplication.java
+++ src/main/java/com/activenture/mib/MibApplication.java
@@ -2,6 +2,7 @@ package com.activenture.mib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,4 +11,10 @@ public class MibApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MibApplication.class, args);
 	}
+	
+	
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(MibApplication.class);
+	  }
 }
